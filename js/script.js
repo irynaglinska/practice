af241c7594d633ae4f36e28b00d2dc07f8fe5fc4
irynaglinska <irasimak95@gmail.com>
2022-10-03 01:09:53 +0000

    added

--- js/script.js
+++ js/script.js
@@ -21,10 +21,10 @@ const isMobile = {
 if (isMobile.any()) {
     document.body.classList.add('_touch');
     let menuArrows = document.querySelectorAll('.menu__arrow');
-    if (menuArrows.length>0) {
+    if (menuArrows.length > 0) {
         for (let index = 0; index < menuArrows.length; index++) {
             const menuArrow = menuArrows[index];
-            menuArrow.addEventListener("click", function(e) {
+            menuArrow.addEventListener("click", function (e) {
                 menuArrow.parentElement.classList.toggle('_active');
             });
         }
